75118226fcd5668900a354f2ae613bc7dcdf7dc4
milky720 <729464040@qq.com>
2025-05-06 20:27:59 +0800

    Add GET and READ operations to TupleSpace

--- tuplespace.py
+++ tuplespace.py
@@ -15,7 +15,7 @@ class TupleSpace:
         }
 
     def put(self, key, value):
-        """Add a key-value pair"""
+        """Add a key-value tuple"""
         with self.lock:
             self.stats['total_operations'] += 1
             self.stats['puts'] += 1
@@ -24,3 +24,24 @@ class TupleSpace:
                 return (False, f"ERR {key} already exists")
             self.store[key] = value
             return (True, f"OK ({key}, {value}) added")
+
+    def get(self, key):
+        """Fetch and remove the key-value tuple"""
+        with self.lock:
+            self.stats['total_operations'] += 1
+            self.stats['gets'] += 1
+            if key not in self.store:
+                self.stats['errors'] += 1
+                return (False, f"ERR {key} does not exist")
+            value = self.store.pop(key)
+            return (True, f"OK ({key}, {value}) removed")
+
+    def read(self, key):
+        """Read the key-value tuple"""
+        with self.lock:
+            self.stats['total_operations'] += 1
+            self.stats['reads'] += 1
+            if key not in self.store:
+                self.stats['errors'] += 1
+                return (False, f"ERR {key} does not exist")
+            return (True, f"OK ({key}, {self.store[key]}) read")
